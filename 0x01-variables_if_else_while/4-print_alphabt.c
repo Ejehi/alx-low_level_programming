32ef90ea85a12000db9e1457d4b4771f48fb808f
Ejehi <ijezry@gmail.com>
2023-04-21 14:22:50 -0700

    When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet[52] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		if (i == "q" || i == "e")
+		{
+			continue;
+		}
+		putchar(alphabet[i]);
+	}
+	putchar('\n');
+	return (0);
+}
