231406121db94f24d2df36f9dbc7ce20c2fba979
Ejehi <ijezry@gmail.com>
2023-04-25 07:20:52 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Print _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
