cf07ee3c1e9a78e0101ed9f2ae5a347ec30562b7
Ejehi <ijezry@gmail.com>
2023-04-26 08:37:38 -0700

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_to_98 - prints all natural numbers from n to 98
+ *
+ *@n: number to be checked
+ *
+ *Return: void
+ */
+void print_to_98(int n)
+{
+	if (n <= 0)
+	{
+		n = 1;
+	}
+	while (n <= 98)
+	{
+		printf("%d, ", n);
+		n++;
+	}
+	printf("\n");
+}
