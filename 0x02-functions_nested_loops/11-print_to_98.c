9da982df3885b97659e3753ccfab671f86dca7ac
Ejehi <ijezry@gmail.com>
2023-04-26 09:05:03 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,11 +12,11 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		for (; n<=98; n++)
+		for (; n <= 98; n++)
 		{
 			if (n == 98)
 			{
-				printf("%d",n);
+				printf("%d", n);
 				printf("\n");
 				break;
 			}
@@ -28,18 +28,17 @@ void print_to_98(int n)
 	}
 	else
 	{
-                for (; n>=98; n--)
-                {
-                        if (n == 98)
-                        {
-                                printf("%d",n);
-                                printf("\n");
-                                break;
-                        }
-                        else
-                        {
-                                printf("%d, ", n);
-                        }
-                }
-        }
+		for (; n >= 98; n--)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			else
+			{
+				printf("%d, ", n);
+			}
+			}
+		}
 }
