91330bf3882c4af42afce7ebe59a178aef9fe9c4
Ejehi <ijezry@gmail.com>
2023-04-28 12:49:30 +0100

    Collaboration is multiplication mandatory

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- *mul - multiplies two integers
- *@a: first integer
- *@b: second integer
- *
- *Return computed multiplication
- */
+  * mul - Multiplies two integers
+  * @a: The first number to multiply
+  * @b: The second number to multiply with first number
+  *
+  * Return: The result of multiplication between x param and y param
+  */
 int mul(int a, int b)
 {
 	return (a * b);
