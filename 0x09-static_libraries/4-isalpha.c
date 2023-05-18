dc4a710cb3e642992d6710dd30a710a2f049b3e3
Ejehi <ijezry@gmail.com>
2023-05-18 19:50:55 +0100

    A library is not a luxury but one of the necessities of life

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *_isalpha -  checks for alphabetic character
+ *
+ *@c: character to check
+ *
+ *Return: (1) if  lowercase or uppercase and (0) otherwise
+ */
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
