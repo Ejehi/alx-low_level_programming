a79a40fadaf2b9ea35ce25e00c75f8ca8534fbbd
Ejehi <ijezry@gmail.com>
2023-05-22 09:05:30 -0700

    File name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -3,10 +3,10 @@
 
 /**
  * main - program that prints the name of the file it was compiled from
- * 
+ *
  * Return: Always (0) on success
  */
-int main()
+int main(void)
 {
 	printf("%s\n", _FILE_);
 	return (0);
